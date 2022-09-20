703f7d2a6ef2943beca7604a74eb0a1cd4ccff6b
Allison Ause <allison.ause@gmail.com>
2022-09-20 11:18:48 -0700

    passing test for getAverageAge

--- reduce.js
+++ reduce.js
@@ -2,20 +2,21 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-import { brands } from './data';
+import { brands, dogs } from './data';
 
 export const getSneaks = (brands) => {
-  //call reduce on brands to get each brand.'
   const shoesArray = brands.map((x) => x.shoes);
   return shoesArray.reduce((flat, next) => flat.concat(next), []);
 };
-//then put each brand's shoes into an accumulated array.
-//could spread shoes array into the push
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const ages = dogs.map((dog) => dog.age);
+  const ageSum = ages.reduce((acc, age) => acc + age, 0);
+  return Number(ageSum / ages.length);
+};
 
 ///possible answer for 1:
 // const sneakers = [];
